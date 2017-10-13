859251292c075eae1b1f93ab3aa6620e59f8e540
Chaniwat Seangchai <chaniwat.meranote@gmail.com>
2017-10-14 01:57:39 +0700

    recompile assets

--- application/public/2017/js/scroll.js
+++ application/public/2017/js/scroll.js
@@ -1,1 +1,97 @@
-!function(t){function e(o){if(n[o])return n[o].exports;var r=n[o]={i:o,l:!1,exports:{}};return t[o].call(r.exports,r,r.exports,e),r.l=!0,r.exports}var n={};e.m=t,e.c=n,e.d=function(t,n,o){e.o(t,n)||Object.defineProperty(t,n,{configurable:!1,enumerable:!0,get:o})},e.n=function(t){var n=t&&t.__esModule?function(){return t.default}:function(){return t};return e.d(n,"a",n),n},e.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},e.p="",e(e.s=4)}({4:function(t,e,n){t.exports=n(5)},5:function(t,e){$(document).on("click",'a[href^="#"]',function(t){var e=$(this).attr("href");if(t.preventDefault(),"/"==window.location.pathname)$("html, body").stop().animate({scrollTop:$($.attr(this,"href")).offset().top},800,"swing");else{var n="/"+e;window.location.replace(n)}})}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 4:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(5);
+
+
+/***/ }),
+
+/***/ 5:
+/***/ (function(module, exports) {
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    var direct = $(this).attr('href');
+    event.preventDefault();
+
+    var current = window.location.pathname;
+    if (current == '/') {
+        $('html, body').stop().animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 800, 'swing');
+    } else {
+        var url = '/' + direct;
+        console.log(url);
+        window.location.replace(url);
+    }
+});
+
+/***/ })
+
+/******/ });
--- application/webpack.mix.js
+++ application/webpack.mix.js
@@ -16,14 +16,14 @@ mix.options({
 });
 
 // mix.js('resources/assets/js/app.js', 'public/js');
-mix.sass("resources/assets/sass/backend.scss", "public/css");
-mix.sass("resources/assets/sass/app.scss", "public/css");
-mix.js("resources/assets/js/landing.js", "public/js");
-mix.js("resources/assets/js/scroll.js", "public/js");
+mix.sass("resources/assets/sass/backend.scss", "public/2017/css");
+mix.sass("resources/assets/sass/app.scss", "public/2017/css");
+mix.js("resources/assets/js/landing.js", "public/2017/js");
+mix.js("resources/assets/js/scroll.js", "public/2017/js");
 
-mix.copy("node_modules/jquery/dist/jquery.min.js", 'public/js');
-mix.copy("node_modules/bootstrap/dist/js/bootstrap.min.js", "public/js");
-mix.copy("node_modules/popper.js/dist/umd/popper.min.js", "public/js");
-mix.copy("node_modules/list.js/dist/list.min.js", "public/js");
-mix.copy("resources/assets/js/kube.min.js", "public/js");
-mix.copy("resources/assets/js/jquery-ui.js", "public/js");
+mix.copy("node_modules/jquery/dist/jquery.min.js", 'public/2017/js');
+mix.copy("node_modules/bootstrap/dist/js/bootstrap.min.js", "public/2017/js");
+mix.copy("node_modules/popper.js/dist/umd/popper.min.js", "public/2017/js");
+mix.copy("node_modules/list.js/dist/list.min.js", "public/2017/js");
+mix.copy("resources/assets/js/kube.min.js", "public/2017/js");
+mix.copy("resources/assets/js/jquery-ui.js", "public/2017/js");
