bd603b86f988087c23a0028d8803f9975ab492ce
Chaniwat Seangchai <chaniwat.meranote@gmail.com>
2017-10-14 01:16:01 +0700

    Add d_url() for use defined url (replaced url() helpers)

--- application/app/Helper/helpers.php
+++ application/app/Helper/helpers.php
@@ -9,6 +9,19 @@ if(!function_exists('d_asset')) {
      */
     function d_asset($path)
     {
+        return d_url($path);
+    }
+}
+
+if(!function_exists('d_url')) {
+    /**
+     * Generate an path for the application by using define url in .env.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function d_url($path)
+    {
         if(substr($path, 0, 1) === '/') {
             $path = substr($path, 1, strlen($path) - 1);
         }
